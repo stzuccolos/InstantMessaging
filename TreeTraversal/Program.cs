111286bf41a268060cdea844a7d4bc3cb49ccead
Zurab Zurabishvili <accs.zukazurabishvili@gmail.com>
2024-02-15 01:02:50 +0400

    add tree traversal

---
+++ TreeTraversal/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TreeTraversal;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var treeRoot = new TreeNode
+        (
+            1,
+            [
+                new
+                (
+                    2,
+                    [
+                        new (3, []),
+                        new (4, [])
+                    ]
+                ),
+                new (5, [])
+            ]
+        );
+
+        var leaves = treeRoot.GetLeaves();
+        foreach (var leave in leaves)
+        {
+            Console.WriteLine(leave.Payload);
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ TreeTraversal/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeTraversal;
+
+public class TreeNode(int payload, List<TreeNode> children)
+{
+    public int Payload { get; set; } = payload;
+    public List<TreeNode> Children { get; set; } = children;
+
+    public IEnumerable<TreeNode> GetLeaves()
+    {
+        var leaves = new List<TreeNode>();
+        GetLeaves(this, leaves);
+        return leaves;
+    }
+
+
+    private void GetLeaves(TreeNode root, List<TreeNode> leaves)
+    {
+        if (!root.Children.Any())
+        {
+            leaves.Add(root);
+            return;
+        }
+
+        foreach (TreeNode child in root.Children)
+        {
+            GetLeaves(child, leaves);
+        }
+    }
+}
+
